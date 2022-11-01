b8e316b8a41163c5a791c364a9e1422d8f1cdfa0
Aleksandr Kharitonovich <harison-din@yandex.ru>
2022-11-01 14:18:57 +0300

    Initial Commit

---
+++ src/main/java/ru/netology/Movie.java
@@ -0,0 +1,13 @@
+package ru.netology;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+
+public class Movie {
+    private int id;
+    private String name;
+    private String movieGenre;
+    private boolean tomorrowPremiere;
+
+}
